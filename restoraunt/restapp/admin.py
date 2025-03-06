c72964cb666d81d5333f0d8b5b0c6d986a3f09d4
Vladimir Zaulin <dream9h@gmail.com>
2025-03-07 02:30:29 +0300

    models were registered

--- restoraunt/restapp/admin.py
+++ restoraunt/restapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from  .models import FoodCategory, Food, FoodSerializer, FoodListSerializer
 # Register your models here.
+
+admin.site.register(FoodCategory)
+admin.site.register(Food)
+admin.site.register(FoodSerializer)
+admin.site.register(FoodListSerializer)
