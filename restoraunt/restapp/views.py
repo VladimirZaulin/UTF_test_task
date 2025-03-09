de891ecc52f49ba2c6e5b6e3b8f693573eac68dc
Vladimir Zaulin <dream9h@gmail.com>
2025-03-10 02:30:52 +0300

    Task Completed

--- restoraunt/restapp/views.py
+++ restoraunt/restapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.db.models import Prefetch, Count, Q
+from rest_framework import generics
+from .models import Food, FoodCategory
+from .serializers import FoodSerializer, FoodListSerializer
 
-# Create your views here.
+
+
+class FoodAPIView(generics.ListAPIView):
+    serializer_class = FoodListSerializer
+    def get_queryset(self):
+        return FoodCategory.objects.prefetch_related().filter(food__is_publish=True).distinct()
+
+
+# class FoodAPIView(generics.ListAPIView):
+#     serializer_class = FoodListSerializer
+#     def get_queryset(self):
+#         """ Выборка только опубликованных блюд(но публикует ещё и пустые категории)"""
+#         published_foods = Food.objects.filter(is_publish=True)
+#         return FoodCategory.objects.prefetch_related(Prefetch('food', queryset=published_foods))
